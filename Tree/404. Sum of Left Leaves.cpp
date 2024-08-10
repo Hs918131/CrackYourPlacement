50ac3befd507cc521aa27210fed4e028445a6982
Harshit Sharma <91822610+Hs918131@users.noreply.github.com>
2024-08-10 21:57:20 +0530

    Create 404. Sum of Left Leaves.cpp

---
+++ Tree/404. Sum of Left Leaves.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    
+    int solve(TreeNode* curr, bool isLeft) {
+        if(!curr) {
+            return 0;
+        }
+        
+        if(!curr->left && !curr->right && isLeft) {
+            return curr->val;
+        }
+        
+        return solve(curr->left, true) + solve(curr->right, false);
+    }
+    
+    int sumOfLeftLeaves(TreeNode* root) {
+        return solve(root, false);
+    }
+};
