1e7aae7efc1d6c93c88e4213dbd985bc86854606
Harshit Sharma <91822610+Hs918131@users.noreply.github.com>
2024-08-21 23:06:32 +0530

    Create GFG_Preorder to BST.cpp

---
+++ Tree/GFG_Preorder to BST.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    Node *helper(int pre[],int &n,int &index,int l,int h)
+    {
+        if(index >= n) return NULL;
+        
+        if(l > pre[index] or pre[index] > h)
+        {
+            return NULL;
+        }
+        
+        Node *root= newNode(pre[index++]);
+        root->left = helper(pre,n,index,l,root->data);
+        root->right = helper(pre,n,index,root->data,h);
+        
+        return root;
+    }
+    Node* Bst(int pre[], int n) {
+       
+        if(n ==0) return NULL;
+        int i=0;
+        return helper(pre,n,i,0,INT_MAX);
+    }
+};
